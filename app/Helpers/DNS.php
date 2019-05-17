eba913623b7a5a19e5d2cfec714d51a5fb4da3df
Lean Sibal <lsibal@apdgroup.com>
2019-05-17 14:19:38 +0800

    Intelligent domain parser

--- app/Helpers/DNS.php
+++ app/Helpers/DNS.php
@@ -2,11 +2,22 @@
 
 namespace App\Helpers;
 
+use LayerShifter\TLDExtract\Extract;
+
 class DNS {
 
     public static function get_mx_records( $domain )
     {
         getmxrr( $domain, $mxhosts, $mxweight );
         return $mxhosts;
+    }
+
+    public static function get_domain( $url )
+    {
+        $extract = new Extract();
+        $extracted = $extract->parse( $url );
+        if( empty( $extracted['hostname'] ) or empty( $extracted['suffix'] ) )
+            return false;
+        return $extracted['hostname'] . '.' . $extracted['suffix'];
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,19 @@
     {{ config('app.name', 'Laravel') }}
     <form method="GET" action="{{ URL::to('/') }}">
         <div class="input-group">
-            <input name="q" type="text" class="form-control" placeholder="Enter domain here ( e.g. http://www.example.com )" value="{{ !empty( $url ) ? $url : '' }}">
+            <input name="q" type="text" class="form-control" placeholder="Enter domain here ( e.g. http://www.example.com or john@appleseed.com )" value="{{ !empty( $url ) ? $url : '' }}">
             <div class="input-group-append">
-                <a class="btn btn-primary" href="#">Search</a>
+                <button class="btn btn-primary" type="submit">Search</button>
             </div>
         </div>
     </form>
 </div>
-@if( !empty( $domain ) )
 <div class="m-b-md mt-5">
-    @if( !empty( $is_google ) )
+    @if( empty( $domain ) && !empty( $url ) )
+        <h1 class="text-danger"><strong>{{ $url }}</strong> is not a valid domain.</h1>
+    @elseif( !empty( $domain ) && !empty( $is_google ) )
         <h1 class="text-success">The domain <strong>{{ $domain }}</strong> is using Google Suite.</h1>
-    @else
+    @elseif( !empty( $domain ) )
         <h1 class="text-danger">The domain <strong>{{ $domain }}</strong> is not using Google Suite.</h1>
     @endif
 
@@ -29,5 +30,4 @@
     </ul>
     @endif
 </div>
-@endif
 @endsection
